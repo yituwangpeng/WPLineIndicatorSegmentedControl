39c11dcb93b05addb8e4862266d6d671b1064f48
wangpeng <yituwangpeng@gmail.com>
2015-10-10 14:46:22 +0800

    Initial Commit

---
+++ WPLineIndicatorSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WPLineIndicatorSegmentedControl
+//
+//  Created by 王鹏 on 15/10/10.
+//  Copyright © 2015年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
